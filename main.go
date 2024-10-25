15fd22f9d7604e5753548ab84a84a19565c97cf3
Kaleab Alemayehu <cipherlofi04@gmail.com>
2024-10-25 13:40:23 +0300

    fix merge conflict

--- main.go
+++ main.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
-	"os/exec"
 )
 
 func main() {
-	listner, err := net.Listen("tcp", ":8080")
+	listener, err := net.Listen("tcp", ":20080")
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatalln("Unable to bind to port!")
 	}
-	cmd := exec.Command("/bin/sh", "-i")
+	log.Println("Listening on 0.0.0.0:20080")
 	for {
-		conn, err := listner.Accept()
+		con, err := listener.Accept()
+		log.Println("Received connection")
 		if err != nil {
-			log.Fatalln(err.Error())
+			log.Fatalln("Unable to accept connection")
 		}
-		cmd.Stdin = conn
-		cmd.Stdout = conn
-		if err := cmd.Run(); err != nil {
-			log.Fatalln(err.Error())
+
+		go echo(con)
+	}
+}
+
+func echo(conn net.Conn) {
+	defer conn.Close()
+	b := make([]byte, 512)
+	for {
+		size, err := conn.Read(b[0:])
+		if err == io.EOF {
+			log.Println("Client disconnected")
+			break
+		}
+		if err != nil {
+			log.Println("Unexpected Error")
+			break
+		}
+		log.Printf("Received %d bytes: %s", size, string(b))
+		log.Println("Writing data")
+		if _, err := conn.Write(b[0:size]); err != nil {
+			log.Fatalln("Unable to Write data")
 		}
 	}
 }
